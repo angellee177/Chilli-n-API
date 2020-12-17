545c1995c141c6d2a236c8c741f2c26878613adc
angellee177 <angelriapurnamasari17@gmail.com>
2020-12-17 13:27:52 +0700

    fix minor unit test

--- tests/routes/index.test.js
+++ tests/routes/index.test.js
@@ -10,7 +10,7 @@ describe("Testing fetching External API and Search Images by Tags", () => {
     
     // Test Case 1, Get all Images Feed List from Flickr API
     describe('/GET public image list from Flickr public feed', () => {
-        it("Should get all public image list from Flickr public feed", function (done) {
+        it("Should get all public image list from Flickr public feed", (done) => {
             chai.request(server)
             .get('/api/v1/feed/list')
             .end(function(err, res) {
